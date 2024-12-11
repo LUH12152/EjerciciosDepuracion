6c34075d6b3222d5d097384741bd5fb07f428a46
joelmr <LUH12152@educastur.es>
2024-12-11 18:02:17 +0100

    examen

--- src/factorial/JoelMataRodriguez/FactorialJoelMataRodriguez.java
+++ src/factorial/JoelMataRodriguez/FactorialJoelMataRodriguez.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class FactorialJoelMataRodriguez {
 
 	public static void main(String[] args) {
-		
+		//TODO:realizar cambios//
 		// Crear un escáner para leer la entrada del usuario
 		Scanner scanner=new Scanner(System.in);
 
